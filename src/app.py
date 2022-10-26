a70b1dd135f4d8f89183236d50809e5ad109f323
Gabriel Belisario <gabrielbelisariof@gmail.com>
2022-10-25 23:26:11 -0400

    custom error pages

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# get the connection to mongoDB through a client (this should be a variable... i guess)
 def get_db():
     client = MongoClient(host='test_mongodb',
                          port=27017,
@@ -13,10 +14,12 @@ def get_db():
     db = client["users_db"]
     return db
 
+# home route
 @app.route('/')
 def ping_server():
     return render_template("index.html")
 
+# demo for fetching mongoDB data
 @app.route('/listUsers')
 def fetch_users():
     db=""
@@ -30,6 +33,16 @@ def fetch_users():
     finally:
         if type(db)==MongoClient:
             db.close()
+
+# file not found
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>custom 404 error page</h1>", 404
+
+# internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return "<h1>custom 500 error page</h1>", 500
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
