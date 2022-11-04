4d059798e81fd3d5fc914f58d8cf5837f5016dfd
Gabriel Belisario <gabrielbelisariof@gmail.com>
2022-11-04 02:13:26 -0400

    select chat working in desktop

--- src/static/js/chat.js
+++ src/static/js/chat.js
@@ -1,2 +1,24 @@
-let chat_container = $("#chat-container");
-chat_container.scrollTop(chat_container.height());
+function selectChat(id_contact) {
+    
+    $(".contact").css("background-color", "white");
+    $("#"+id_contact).css("background-color", "#eee");
+
+    $( "#chat-div-container" ).removeClass("d-none");
+    $( "#chat-div-container" ).addClass("d-block");
+
+    $(".messageList").removeClass("d-block");
+    $(".messageList").addClass("d-none");
+
+    let chat_element = $( "#chat-"+ id_contact );
+    chat_element.removeClass("d-none");
+    chat_element.addClass("d-block");
+    chat_element.scrollTop( chat_element.height() );
+
+}
+
+$(".contact").each( (idx)=>{
+        let id_contact = $(".contact")[idx].id;
+        console.log( id_contact ) ;
+        $("#"+id_contact ).click( () => selectChat(id_contact) );
+    } 
+);
