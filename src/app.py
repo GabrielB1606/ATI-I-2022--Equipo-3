b81ad4a1239dcd822c547d94332a60550259e659
ruggieroDaniela <chachy.drs@gmail.com>
2022-11-27 16:59:36 -0400

    login with facebook

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, url_for, redirect
 import pymongo
 from pymongo import MongoClient
+from authlib.integrations.flask_client import OAuth
 import json
+import os
 
 app = Flask(__name__)
+app.secret_key = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O/<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
+oauth = OAuth(app)
 
 # get the connection to mongoDB through a client (this should be a variable... i guess)
 def get_db():
@@ -146,6 +150,34 @@ def fetch_users():
     finally:
         if type(db)==MongoClient:
             db.close()
+
+@app.route('/facebook/')
+def facebook():
+    # Facebook Oauth Config
+	FACEBOOK_CLIENT_ID = '690747602573297'
+	FACEBOOK_CLIENT_SECRET = '8936f7e5d6fc5dda0056b58bcb85bc54'
+	oauth.register(
+		name='facebook',
+		client_id=FACEBOOK_CLIENT_ID,
+		client_secret=FACEBOOK_CLIENT_SECRET,
+		access_token_url='https://graph.facebook.com/oauth/access_token',
+		access_token_params=None,
+		authorize_url='https://www.facebook.com/dialog/oauth',
+		authorize_params=None,
+		api_base_url='https://graph.facebook.com/',
+		client_kwargs={'scope': 'email'},
+	)
+	redirect_uri = url_for('facebook_auth', _external=True)
+	return oauth.facebook.authorize_redirect(redirect_uri)
+
+@app.route('/facebook/auth/')
+def facebook_auth():
+	token = oauth.facebook.authorize_access_token()
+	resp = oauth.facebook.get(
+		'https://graph.facebook.com/me?fields=id,name,email,picture{url}')
+	profile = resp.json()
+	print("Facebook User ", profile)
+	return redirect('/')
 
 # file not found
 @app.errorhandler(404)
