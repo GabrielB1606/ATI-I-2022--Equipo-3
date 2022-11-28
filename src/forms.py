908b5dfe8b68cbac3c3d92c6db122a1355e56ab2
gabrielLiendo <gabo.c.liendo@gmail.com>
2022-11-28 10:12:59 -0400

    internalicionalize input errors

--- src/forms.py
+++ src/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, TextAreaField, DateField, SubmitField)
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Length, EqualTo
 from datetime import date
 
 class RegisterForm(FlaskForm):
     email = StringField(validators=[InputRequired(message="Introduzca un correo")])
-    password = StringField(validators=[InputRequired(), Length(min=7)])
+    password = StringField(validators=[InputRequired(), EqualTo('confirm'), Length(min=7)])
+    confirm = StringField()
     name = StringField(validators=[InputRequired()])
     biography = TextAreaField()
     birthday = DateField(default=date.today(), format='%Y-%m-%d')
