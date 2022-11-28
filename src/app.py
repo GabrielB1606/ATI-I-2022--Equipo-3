ec44ed8171db64abe61fa5f564f35440263c0d72
Miguel Valera <jhoabran@gmail.com>
2022-11-28 15:45:52 -0400

    this works

--- src/app.py
+++ src/app.py
@@ -74,6 +74,7 @@ def user_login():
     found = database_hook.usuarios.find_one({ "email": request.form.get("email"), "clave": request.form.get("password") }) 
     
     if found:
+        
         return User().login(),200
 
     return "",200
--- src/models.py
+++ src/models.py
@@ -5,14 +5,13 @@ class User:
     def start_session(self,user):
         session['logged_in'] = True
         session['user'] = user
-        return jsonify(user),200
+        return jsonify(user)
 
     def login(self):
         user={
             "email": request.form.get('email')
         }
-        self.start_session(user)
-        return "done"
+        return self.start_session(user)
         
 
     def signup(self):
