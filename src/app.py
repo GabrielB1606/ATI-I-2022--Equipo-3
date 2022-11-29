404c86f375ab3588d1f34bce829fe14fdff3ac5b
Gabriel Belisario <gabrielbelisariof@gmail.com>
2022-11-29 13:59:41 -0400

    user view module

--- src/app.py
+++ src/app.py
@@ -21,6 +21,9 @@ app.register_blueprint(chat, url_prefix="")
 from views.images import image_collection
 app.register_blueprint(image_collection, url_prefix="/img")
 
+from views.user import users
+app.register_blueprint(users, url_prefix="/user")
+
 # profile route
 @app.route('/friend')
 def profileFriend():
@@ -40,25 +43,6 @@ def profileFriend():
 @app.route('/user')
 def profile():
     return redirect("/user/chachy.drs@mail.com")
-
-# profile route
-@app.route('/user/<email>')
-def profileUser(email):
-    # find user info mongodb
-    user_info = database_hook["usuarios"].find_one( {"email": email} )["perfil"]
-    posts = json.load( open("data/dummy/posts_friend.json") )
-    # read GET variable
-    lan = request.args.get("lang")
-    if lan == "es":
-        # open config file according to the GET variable lang
-        lang = json.load( open("static/config/es/index.json") )
-        config = json.load(open("static/config/es/config.json"))
-    else:
-        lang = json.load( open("static/config/en/index.json") )
-        config = json.load(open("static/config/en/config.json"))
-    get_navbar_lang(lang)
-    # return user_info
-    return render_template("profile.html", postList = posts, lang=lang, language=lan, config=config, user_info=user_info)
 
 # config route
 @app.route('/config')
---
+++ src/views/user.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request, render_template
+from config import database_hook, get_navbar_lang
+import json
+
+users = Blueprint("users", __name__, static_folder="static", template_folder="templates")
+
+# demo for fetching mongoDB data
+@users.route('/<email>')
+def profileUser(email):
+    # find user info mongodb
+    user_info = database_hook["usuarios"].find_one( {"email": email} )["perfil"]
+    posts = json.load( open("data/dummy/posts_friend.json") )
+    # read GET variable
+    lan = request.args.get("lang")
+    if lan == "es":
+        # open config file according to the GET variable lang
+        lang = json.load( open("static/config/es/index.json") )
+        config = json.load(open("static/config/es/config.json"))
+    else:
+        lang = json.load( open("static/config/en/index.json") )
+        config = json.load(open("static/config/en/config.json"))
+    get_navbar_lang(lang)
+    # return user_info
+    return render_template("profile.html", postList = posts, lang=lang, language=lan, config=config, user_info=user_info)
