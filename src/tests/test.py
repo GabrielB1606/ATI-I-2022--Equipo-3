185dcc9fdba7e611adbbb8d60d94440ef1cb3420
ruggieroDaniela <chachy.drs@gmail.com>
2022-11-29 17:55:58 -0400

    unit test login facebook

--- src/tests/test.py
+++ src/tests/test.py
@@ -1,5 +1,5 @@
 from . import BaseTestClass
-
+from flask import url_for, request
 # python -m unittest
 
 class TestHello(BaseTestClass):
@@ -8,3 +8,8 @@ class TestHello(BaseTestClass):
         rv = self.app.get('/login')
         self.assertEqual(rv.status, '200 OK')
         # self.assertEqual(rv.data, b'Hello World!\n')
+
+    def test_loginFacebook(self):
+        rv = self.app.get('/facebook')
+        self.assertEqual(rv.status, '308 PERMANENT REDIRECT')
+
