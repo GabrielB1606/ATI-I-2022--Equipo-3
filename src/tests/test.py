6fc2c0c06046981b6db7844ad85db71a6556a0e1
gabrielLiendo <gabo.c.liendo@gmail.com>
2022-11-29 17:41:47 -0400

    register and facebook tests

--- src/tests/test.py
+++ src/tests/test.py
@@ -1,10 +1,55 @@
 from . import BaseTestClass
+from config import database_hook
 
 # python -m unittest
 
 class TestHello(BaseTestClass):
 
-    def test_hello(self):
-        rv = self.app.get('/login')
-        self.assertEqual(rv.status, '200 OK')
-        # self.assertEqual(rv.data, b'Hello World!\n')
+    def test_register_incorrect(self):
+        response = self.app.post('/sign_in', data={
+            'email':'test@mail.com',
+            'password':'12345678',
+            'confirm':'12345678',
+            'name':''
+        }, follow_redirects = True)
+
+        user = database_hook["usuarios"].find_one({'email':'test@mail.com'})
+
+        self.assertEqual(user, None)
+        self.assertEqual(response.request.path, '/sign_in')
+
+    def test_register_correct(self):
+        response = self.app.post('/sign_in', data={
+            'email':'test@mail.com',
+            'password':'12345678',
+            'confirm':'12345678',
+            'name':'Johny Test'
+        }, follow_redirects = True)
+
+        user = database_hook["usuarios"].find_one({'email':'test@mail.com'})
+
+        self.assertNotEqual(user, None)
+        self.assertEqual(user['email'], 'test@mail.com')
+        self.assertEqual(response.request.path, '/login')
+
+        database_hook["usuarios"].delete_one({'email':'test@mail.com'})
+
+    def test_login_correct(self):
+        response = self.app.post('/sign_in', data={
+            'email':'test@mail.com',
+            'password':'12345678',
+            'confirm':'12345678',
+            'name':'Johny Test'
+        }, follow_redirects = True)
+
+        user = database_hook["usuarios"].find_one({'email':'test@mail.com'})
+
+        self.assertNotEqual(user, None)
+        self.assertEqual(user['email'], 'test@mail.com')
+        self.assertEqual(response.request.path, '/login')
+
+        database_hook["usuarios"].delete_one({'email':'test@mail.com'})
+
+    def test_loginFacebook(self):
+        rv = self.app.get('/facebook')
+        self.assertEqual(rv.status, '308 PERMANENT REDIRECT')
