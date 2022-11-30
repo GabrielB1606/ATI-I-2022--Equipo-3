84966980efa2940d1f7141cd213f98591b30ad57
ruggieroDaniela <chachy.drs@gmail.com>
2022-11-30 12:29:43 -0400

    login password: clave incorrecta, usuario invalido

--- tests-selenium/test_loginFacebook.py
+++ tests-selenium/test_loginFacebook.py
@@ -28,6 +28,48 @@ class testLoginFabook(unittest.TestCase):
         time.sleep(30)
         self.assertTrue( self.driver.current_url.split("/")[-1] != "login" , "No se inició sesión correctamente" )
 
+    def test_loginFacebook_clave_incorrecta(self):
+        self.driver.get("http://localhost:5000/login")              # Open Chrome and go to http://localhost:5000/login
+
+        # Click Facebook Button
+        facebook = self.driver.find_element(By.ID, "facebookButton")
+        facebook.send_keys(Keys.ENTER)
+
+        # Type Email 
+        email = self.driver.find_element(By.ID, "email")
+        email.send_keys("Chachy.drs@gmail.com")
+
+        # Type Password 
+        password = self.driver.find_element(By.ID, "pass")
+        password.send_keys("123456")
+
+        # Send 
+        password.send_keys(Keys.ENTER)
+        time.sleep(30)
+        
+        self.assertTrue( self.driver.current_url.find("facebook") , "No se inició sesión correctamente" )
+
+    def test_loginFacebook_usuario_incorrecto(self):
+        self.driver.get("http://localhost:5000/login")              # Open Chrome and go to http://localhost:5000/login
+
+        # Click Facebook Button
+        facebook = self.driver.find_element(By.ID, "facebookButton")
+        facebook.send_keys(Keys.ENTER)
+
+        # Type Email 
+        email = self.driver.find_element(By.ID, "email")
+        email.send_keys("fifo")
+
+        # Type Password 
+        password = self.driver.find_element(By.ID, "pass")
+        password.send_keys("123456")
+
+        # Send 
+        password.send_keys(Keys.ENTER)
+        time.sleep(30)
+        
+        self.assertTrue( self.driver.current_url.find("facebook") , "No se inició sesión correctamente" )
+
 # Destructor (it runs after EVERY test)
     def tearDown(self) -> None:
         self.driver.close()
