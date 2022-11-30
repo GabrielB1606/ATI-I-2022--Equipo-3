6a07989891e4717d2d316feb80d7e3bff40817e2
Gabriel Belisario <gabrielbelisariof@gmail.com>
2022-11-30 11:23:09 -0400

    test doc

--- tests-selenium/test_login.py
+++ tests-selenium/test_login.py
@@ -6,26 +6,34 @@ import time
 
 class testLogin( unittest.TestCase ):
     
+    # start driver as an attribute of the class
     def setUp(self):
         self.driver = webdriver.Firefox()
 
     def test_login(self):
+        # go to the login page
         self.driver.get("http://localhost:5000/login")
+        
+        # type email
         usuario = self.driver.find_element( By.ID, "email" )
         usuario.send_keys("chachy.drs@gmail.com")
 
+        # type password
         password = self.driver.find_element( By.ID, "password" )
         password.send_keys("28126743")
 
+        # submit login form
         password.send_keys(Keys.ENTER)
 
+        #wait to load lol
         time.sleep(3)
 
+        # if I'm still in login, i haven't logged in (duh...)
         self.assertTrue( self.driver.current_url.split("/")[-1] != "login" , "Todavía seguimos en la dirección de login, por lo que no se inició sesión correctamente" )
     
+    # destructor 
     def tearDown(self) -> None:
         self.driver.close()
-        pass
 
 if __name__ == "__main__":
     unittest.main()
